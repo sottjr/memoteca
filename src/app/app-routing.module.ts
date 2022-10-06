8a635caa428de9b4de0097039e09507cd9cb252e
Junior Sott <sottjr@gmail.com>
2022-10-06 20:14:10 +0200

    chore: modify routes config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateThinkingComponent } from './components/thinkings/create-thinking/create-thinking.component';
+import { GetThinkingsComponent } from './components/thinkings/get-thinkings/get-thinkings.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "",
+  redirectTo: "get-thinkings",
+  pathMatch: "full"
+  },
+  {path: "create-thinkings",
+  component: CreateThinkingComponent,
+  },
+  {path: "get-thinkings",
+  component: GetThinkingsComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
